6fbdcab6e2594d418953631e8e69ff4c5960e027
xokera709 <148460685+xokera709@users.noreply.github.com>
2023-12-14 21:44:52 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,38 @@
+import random
+    main()
+def main():
+if __name__ == "__main__":
+
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+    for item in data:
+def generate_random_data():
+
+
+
+    data = generate_random_data()    }
+        for (int i = 0; i < 10; i++) {
+public class RandomDataGenerator {
+
+
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+            System.out.println("Random Number: " + item);
+        int[] data = new int[10];
+}
+        }
+    public static void main(String[] args) {
+        for (int item : data) {        for (int i = 0; i < 10; i++) {
+    }
+
+public class RandomDataGenerator {
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+        }
+}
+
+        int[] data = new int[10];
+            System.out.println("Random Number: " + item);
+        for (int item : data) {
+    public static void main(String[] args) {
